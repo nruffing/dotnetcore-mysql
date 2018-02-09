c30573306744533271afab36363e4c6ba12e782a
nick <nicholasruffing70@gmail.com>
2018-02-08 22:56:54 -0500

    Create data access builder

---
+++ DotNetCoreMySql/DotNetCoreMySql/Dao/Builder/DataAccessBuilderCommand.cs
@@ -0,0 +1,63 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace DotNetCoreMySql.Dao.Builder
+{
+    public class DataAccessBuilderCommand : IDataAccessBuilderCommand
+    {
+        #region Private Members
+
+        MySqlCommand _command;
+
+        #endregion
+
+        #region Construction
+
+        public DataAccessBuilderCommand(MySqlCommand command)
+        {
+            _command = command;
+        }
+
+        #endregion
+
+        #region IDataAccessBuildCommand Implementation
+
+        public IDataAccessBuilderCommand WithInputParameter(string parameterName, MySqlDbType type, object value)
+        {
+            MySqlParameter parameter = new MySqlParameter(parameterName, type);
+            parameter.Value = value;
+            parameter.Direction = ParameterDirection.Input;
+            _command.Parameters.Add(parameter);
+
+            return this;
+        }
+
+        public IDataAccessBuilderCommand WithOutputParameter(string parameterName, MySqlDbType type, object value)
+        {
+            MySqlParameter parameter = new MySqlParameter(parameterName, type);
+            parameter.Direction = ParameterDirection.Output;
+            _command.Parameters.Add(parameter);
+
+            return this;
+        }
+
+        public async Task ExecuteNonQueryAsync()
+        {
+            await _command.ExecuteNonQueryAsync();
+        }
+
+        public async Task<DbDataReader> ExecuteReaderAsync()
+        {
+            return await _command.ExecuteReaderAsync();
+        }
+
+        public async Task<object> ExecuteScalarAsync()
+        {
+            return await _command.ExecuteScalarAsync();
+        }
+
+        #endregion
+    }
+}
---
+++ DotNetCoreMySql/DotNetCoreMySql/Dao/Builder/IDataAccessBuilderCommand.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace DotNetCoreMySql.Dao.Builder
+{
+    public interface IDataAccessBuilderCommand
+    {
+        IDataAccessBuilderCommand WithInputParameter(string parameterName, MySqlDbType type, object value);
+
+        IDataAccessBuilderCommand WithOutputParameter(string parameterName, MySqlDbType type, object value);
+
+        Task ExecuteNonQueryAsync();
+
+        Task<object> ExecuteScalarAsync();
+
+        Task<DbDataReader> ExecuteReaderAsync();
+    }
+}
